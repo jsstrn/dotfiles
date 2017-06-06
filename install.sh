755e205ea4dba3c4df464edd4b3558b797a93c11
Jesstern Rays <jessternrays@gmail.com>
2017-06-06 22:21:33 +0800

    Update install script
    - run brew update, cleanup, and upgrade
    - install Oh My Zsh
    - change shell to Zsh

--- install.sh
+++ install.sh
@@ -10,11 +10,23 @@ fi
 which brew || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor || exit $?
 
+# Update Homebrew
+brew update
+
 # Install Homebrew Bundle
 brew tap 'homebrew/bundle'
 
 # Uninstall dependencies not listed in Brewfile
-brew bundle cleanup
+brew bundle cleanup --force
+
+# Upgrade installed dependencies
+brew upgrade --all
 
 # Check and installs dependencies in Brewfile
 brew bundle check || brew bundle install
+
+# Install Oh My Zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Change shell to Zsh
+chsh -s /bin/zsh
