e04d8532f3ae89038c32edeebf20da545a872016
Jesstern Rays <jsstrn@users.noreply.github.com>
2020-04-27 20:39:01 +0800

    Improve readability with line breaks

--- .scripts/login-items.sh
+++ .scripts/login-items.sh
@@ -2,21 +2,30 @@
 
 # Alfred
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Alfred", path: "/Applications/Alfred 3.app", hidden: false }'
+
 # Amphetamine
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Amphetamine", path: "/Applications/Amphetamine.app", hidden: false }'
+
 # Bartender
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Bartender", path: "/Applications/Bartender 3.app", hidden: false }'
+
 # Fluor
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Fluor", path: "/Applications/Fluor.app", hidden: false }'
+
 # Flux
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Flux", path: "/Applications/Flux.app", hidden: false }'
+
 # Itsycal
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Itsycal", path: "/Applications/Itsycal.app", hidden: false }'
+
 # Micro Snitch
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Micro Snitch", path: "/Applications/Micro Snitch.app", hidden: false }'
+
 # ShiftIt
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "ShiftIt", path: "/Applications/ShiftIt.app", hidden: false }'
+
 # NordVPN
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "NordVPN", path: "/Applications/NordVPN.app", hidden: false }'
+
 # Tomato One
 osascript -e 'tell application "System Events" to make login item at end with properties { name: "Tomato One", path: "/Applications/Tomato One.app", hidden: false }'
