bda21d67a7967342a39c2f881d080c775b9c372c
Jesstern Rays <jsstrn@users.noreply.github.com>
2021-09-05 00:32:45 +0800

    Add SSH key to GitHub

--- .scripts/ssh.sh
+++ .scripts/ssh.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
+# set -x # for debugging; prints executed commands
 set -e
+
+PRIVATE_KEY=~/.ssh/id_rsa
+PUBLIC_KEY=${PRIVATE_KEY}.pub
+
+echo ${PUBLIC_KEY}
+
+function openGitHub {
+  echo -e '\n🔒  Login to to your GitHub\n'
+}
 
 function generateKeys {
   echo -e '\n🔒  Generating new SSH key\n'
@@ -12,16 +22,59 @@ function addKeysToAgent {
   eval "$(ssh-agent -s)"
 
   echo -e '\n🔐  Adding your key to the SSH agent\n'
-  ssh-add -K ~/.ssh/id_rsa
+  # ssh-add -K ~/.ssh/id_rsa
+  ssh-add -K ${PRIVATE_KEY}
 }
 
 function copyKeysToClipboard {
   echo -e '\n📋  Copying your public key to the clipboard\n'
-  cat ~/.ssh/id_rsa.pub | pbcopy
+  # cat ~/.ssh/id_rsa.pub | pbcopy
+  cat ${PUBLIC_KEY} | pbcopy
 }
+
+function isGitHubInstalled {
+  command -v gh &> /dev/null
+}
+
+function promptUserForKeyFileTitle {
+  echo -e '🙋‍♀️  Enter title for key file (e.g. personal computer)'
+  read TITLE 
+}
+
+function addKeyFileToGitHub {
+  echo -e '📦  Adding key file to GitHub\n'
+  gh ssh-key add ${PUBLIC_KEY} --title "${TITLE}"
+}
+
+function listKeysOnGitHub {
+  echo -e '📋  Listing all keys on GitHub\n'
+  gh ssh-key list
+}
+
+function setRemoteUrl {
+  URL="git@github.com:jsstrn/dotfiles.git"
+  git remote set-url origin ${URL}
+}
+
+if ! isGitHubInstalled; then
+  echo -e '📦  Installing the GitHub CLI tool\n'
+  brew install gh
+fi
+
+if ! gh auth status; then
+  echo -e '🔓  Login to GitHub\n'
+  gh auth login
+fi
 
 if [ -f ~/.ssh/id_rsa ]; then
   echo -e '\n🔑  Found existing SSH key\n'
 else
-  generateKeys && addKeysToAgent && copyKeysToClipboard
+  generateKeys && \
+  addKeysToAgent && \
+  copyKeysToClipboard
 fi
+
+promptUserForKeyFileTitle && \
+addKeyFileToGitHub
+listKeysOnGitHub
+setRemoteUrl
